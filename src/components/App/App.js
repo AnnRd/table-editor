8c1b7077a8491eda99399ad5b50e1c17e947de1d
Anna Radchenko <anrd.rs>
2023-01-27 16:24:00 +0300

    create components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import { useState } from "react";
+import { Form } from "../Form/Form";
+import { Table } from "../Table/Table";
+import s from './App.module.css';
 
 function App() {
+  const [user, setUser] = useState([
+    {
+      id: 1, name: 'Кепка', value: '100р'
+    },
+    {
+      id: 2, name: 'Куртка', value: '200р'
+    },
+    {
+      id: 3, name: 'Шляпа', value: '500р'
+    },
+    {
+      id: 4, name: 'Кувалда', value: '333р'
+    }
+  ]);
+
+  const [editableUserData, setEditableUserData] = useState({
+    isEdit: false,
+    userIndex: null,
+  })
+
+  
+  const [inputValue, setInputValue] = useState({
+    name: '',
+    value: '',
+  });
+
   return (
-    <div className="App">
-      
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <Table />
+        <div>
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,5 @@
+import s from './Button.module.css'
+
+export function Button() {
+    return <div></div>
+}
